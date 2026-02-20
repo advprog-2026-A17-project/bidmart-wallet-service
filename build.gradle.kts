d5437ffcff00927b0705c6f57c46248d7717ed1b
Weeleeam <william.jonnatan@ui.ac.id>
2026-02-20 17:06:34 +0700

    Fix failed build (2)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     java
-    jacoco
     id("org.springframework.boot") version "3.5.11"
     id("io.spring.dependency-management") version "1.1.7"
-    id("org.sonarqube") version "5.0.0.4638"
+    id("jacoco")
+    id("org.sonarqube") version "4.4.1.3373"
 }
 
 group = "id.ac.ui.cs.advprog"
@@ -37,34 +37,36 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-sonar {
-    properties {
-        property("sonar.projectKey", "advprog-2026-A17-project_bidmart-wallet-service")
-        property("sonar.organization", "advprog-2026-a17-project")
-        property("sonar.host.url", "https://sonarcloud.io")
-        property(
-            "sonar.coverage.jacoco.xmlReportPaths",
-            "${layout.buildDirectory.get()}/reports/jacoco/test/jacocoTestReport.xml"
-        )
-    }
-}
-
 tasks.withType<Test> {
     useJUnitPlatform()
-}
-
-tasks.test {
-    filter {
-        excludeTestsMatching("*FunctionalTest")
-    }
-    finalizedBy(tasks.jacocoTestReport)
 }
 
 tasks.jacocoTestReport {
     dependsOn(tasks.test)
     reports {
         xml.required.set(true)
-        csv.required.set(false)
         html.required.set(true)
+    }
+}
+
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            limit {
+                minimum = "0.8".toBigDecimal()
+            }
+        }
+    }
+}
+
+tasks.check {
+    dependsOn(tasks.jacocoTestCoverageVerification)
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "advprog-2026-A17-project_bidmart-wallet-service")
+        property("sonar.organization", "advprog-2026-a17-project")
+        property("sonar.host.url", "https://sonarcloud.io")
     }
 }
